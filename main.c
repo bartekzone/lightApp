b011c8f01faf9aeed1583fc4f056d637e2c69242
bartekzone <125757661+bartekzone@users.noreply.github.com>
2025-06-24 14:59:02 +0200

    Added example of callback

--- main.c
+++ main.c
@@ -2,10 +2,29 @@
 main app 
 *******************************************************************************/
 #include "Swc_LightsControl.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+// Callback example
+typedef void (*LightCallback)(void);
+// Funkcja zapalająca światło i informująca kogoś
+void turnOnLight(LightCallback callback)
+{
+    Swc_Lights_TurnOnLed();
+    printf("Light turned on\n");
+
+    if (callback != NULL)
+        callback();  // wywołaj reakcję
+}
+
+// Implementacja reakcji
+void onLightTurnedOn() {
+    printf(">> User has been notified that light is on.\n");
+}
 int main(void) {
 
     Swc_Lights_TurnOnLed();
     Swc_Lights_TurnOffRelay();
+    turnOnLight(onLightTurnedOn);
     return 0;
 }
